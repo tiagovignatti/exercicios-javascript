5b437395e94ff27c0cdd793b1bfee7221fd8356a
flameastro <gabrielgoulartbnu@gmail.com>
2025-10-06 13:15:52 -0300

    Corrigindo a ortografia

--- Exercicios/ex 1.1/js/script.js
+++ Exercicios/ex 1.1/js/script.js
@@ -7,6 +7,6 @@ Dica - exibindo (ou "imprimindo") números na tela:
 
 var numero1 = 2;
 var numero2 = 3; 
-var numeor3 = 5;
+var numero3 = 5;
 
-console.log(numero1, numero2, numeor3);
+console.log(numero1, numero2, numero3);
